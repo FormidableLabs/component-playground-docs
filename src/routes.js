0a8f73bfe6052466bd820b40a38d3eec9cb40e30
ebrillhart <emma.brillhart@gmail.com>
2016-08-19 11:04:29 -0700

    functional routes

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Route, IndexRoute } from "react-router";
 import Home from "./screens/home/index";
-import Guide from "./screens/docs/index";
 
 // Components
 import App from "./components/app";
+import { Guide, Docs } from "./screens/docs/index";
 
 module.exports = (
   <Route path="/" component={App}>
     <IndexRoute component={Home}/>
+    <Route path="/docs" component={Docs}/>
     <Route path="/docs/getting-started" component={Guide}/>
   </Route>
 );
--- src/screens/docs/index.js
+++ src/screens/docs/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Ecology from "ecology";
 import GettingStarted from "../../../node_modules/component-playground/docs/getting-started.md";
+import Api from "../../../node_modules/component-playground/docs/api.md";
 
 class Guide extends React.Component {
   render() {
@@ -15,4 +16,16 @@ class Guide extends React.Component {
   }
 }
 
-export default Guide;
+class Docs extends React.Component {
+  render() {
+    return (
+      <Ecology
+        overview={Api}
+        scope={{React, ReactDOM}}
+        playgroundtheme="elegant"
+      />
+    );
+  }
+}
+
+export { Guide, Docs };
