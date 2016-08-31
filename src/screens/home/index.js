36cb1cee01894ae6312956412ec55b137edf0bec
ebrillhart <emma.brillhart@gmail.com>
2016-08-31 12:04:19 -0700

    updating copy and ready for design

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -14,7 +14,7 @@ class Home extends React.Component {
     return (
       <div>
         <h1>Component Playground</h1>
-        <p>Render React.js components with editable source and live preview</p>
+        <p>Render React.js components with editable source and live preview.</p>
         <pre><code>npm install component-playground</code></pre>
         <iframe src="https://ghbtns.com/github-btn.html?user=formidablelabs&repo=component-playground&type=star&count=true&size=large" frameBorder="0" scrolling="0" width="160px" height="30px"></iframe>
         <Ecology
@@ -26,11 +26,13 @@ class Home extends React.Component {
         <br/>
         <a href="https://github.com/FormidableLabs/component-playground/issues">Report an Issue</a>
         <h2>Features</h2>
-          <ul>
-            <li>Allow editing of source code to illustrate changes</li>
-            <li>Live preview of React components</li>
-            <li>Great for tutorials and walkthroughs</li>
-          </ul>
+          <h3>Allow editing of source code to illustrate changes.</h3>
+            <p>We've used Component Playground to allow people to edit themes and see the changes in real time.
+             Let people see how flexible your software is for themselves.</p>
+          <h3>Live preview of React components.</h3>
+            <p>Upgrade from a gif and show people what's behind-the-scenes on that cool animated component.</p>
+          <h3>Great for tutorials and walkthroughs.</h3>
+            <p>Help users keep track of what's going on at any given point in a guide or tutorial with live code.</p>
         <RadiumLink to="/docs/getting-started">Let&#8217;s Get Started</RadiumLink>
       </div>
     );
